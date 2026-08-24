c125bb8e9a4229797944d49b51d893c3a89cf218
mariobeltrao <mario16bleal@gmail.com>
2026-08-24 17:18:14 -0300

    Day 02 - Tip Calculator

---
+++ 002.py
@@ -0,0 +1,6 @@
+print("Welcome to the tip calculator!")
+total_bill = int(input("What was the total bill?\n"))
+tip = int(input("How much tip would you like to give?\n"))
+people = int(input("How many people to split the bill?\n"))
+average_bill = ((total_bill + tip) / people)
+print(f"Each person should pay: ${average_bill}")
