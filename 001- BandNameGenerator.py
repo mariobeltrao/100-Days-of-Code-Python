2e6011e7dcbc0fd23797d5f7d4f176ed01e21f2e
mariobeltrao <mario16bleal@gmail.com>
2026-08-24 17:04:51 -0300

    Day 01 - Band Name Generator

---
+++ 001- BandNameGenerator.py
@@ -0,0 +1,12 @@
+# username = input("Write your username:") 
+# length_username = len(username)
+# print(username, length_username)
+
+
+print("===================="
+      "Welcome to the Band Name Generator"
+      "====================")
+
+city = input("Wich city did you grew up in?\n")
+name_pet = input("What's your pet's name?\n")
+print(f"Your band name could be {city}" + f" {name_pet}")
